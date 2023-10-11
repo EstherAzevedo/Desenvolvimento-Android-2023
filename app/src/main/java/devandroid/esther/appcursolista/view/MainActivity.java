2401be6947a0139e78a4eec27fe6b9cb965982a0
EstherAzevedo/ <estherazvdo123@gmail.com>
2023-10-10 23:23:46 -0300

    Arquitetura MVC

--- app/src/main/java/devandroid/esther/appcursolista/view/MainActivity.java
+++ app/src/main/java/devandroid/esther/appcursolista/view/MainActivity.java
@@ -1,8 +1,10 @@
-package devandroid.esther.appcursolista;
+package devandroid.esther.appcursolista.view;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import devandroid.esther.appcursolista.R;
 
 public class MainActivity extends AppCompatActivity {
 
